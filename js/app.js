520ae3c06f68f12d97ffff857076dd63374525a6
ajay-bishnoi <AJAY2003BISHNOI@GMAIL.COM>
2024-01-10 16:28:12 +0530

    update 01

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@
 
 function openNav() {
   document.querySelector(".closeNav").classList.toggle("start-0");
-  document.body.classList.toggle(".overflow_hidden");
+  document.body.classList.toggle("overflow_hidden");
   document.querySelector(".menu").classList.toggle("cross");
 }
